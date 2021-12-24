8f4a6a04cc871fee836e131c42151965f2eb55b3
GlebShekovtsov <80450328+GlebShekovtsov@users.noreply.github.com>
2021-12-24 11:07:59 +0300

    syntax commit
    переход к camelcase

--- ZD4.8EX_3/Program.cs
+++ ZD4.8EX_3/Program.cs
@@ -12,33 +12,33 @@ namespace ZD4._8EX_3
         {
             Random rand = new Random();
             Console.Write("Введите максимальный диапазон рандома: ");
-            int rand_range = Convert.ToInt32(Console.ReadLine());
-            int cpu_num = rand.Next(0, rand_range);
-            string user_num;
+            int randRange = Convert.ToInt32(Console.ReadLine());
+            int cpuNum = rand.Next(0, randRange);
+            string userNum;
             int tryes = 0;
             while (true)
             {
                 Console.Write("\nВведите вашу отгадку: ");
-                user_num = Console.ReadLine();
+                userNum = Console.ReadLine();
                 tryes++;
-                if (user_num=="")
+                if (userNum == "")
                 {
-                    Console.WriteLine($"Задуманное число: {cpu_num}");
+                    Console.WriteLine($"Задуманное число: {cpuNum}");
                     Console.ReadLine();
                     break;
                 }
-                if (Convert.ToInt32(user_num) > cpu_num)
+                if (Convert.ToInt32(userNum) > cpuNum)
                 {
                     Console.WriteLine("Задуманное компьютером число меньше! Попробуйте ещё раз");
                 }
-                else if (Convert.ToInt32(user_num)< cpu_num)
+                else if (Convert.ToInt32(userNum) < cpuNum)
                 {
                     Console.WriteLine("Задуманное компьютером число больше! Попробуйте ещё раз");
                 }
 
                 else
                 {
-                    Console.WriteLine($"Вы отгадали, задуманное число: {cpu_num}, число попыток: {tryes}");
+                    Console.WriteLine($"Вы отгадали, задуманное число: {cpuNum}, число попыток: {tryes}");
                     Console.ReadLine();
                     break;
                 }
